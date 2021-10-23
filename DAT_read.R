ae612648d8df0cefc0d9bc9ae1474dde230c84b7
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-10-23 06:20:19 +0200

    Return all data of selected table in raw format
    Title of the selected table is dentified and based on the
    index list all rows are returned between selected and
    next titles.

--- DAT_read.R
+++ DAT_read.R
@@ -4,6 +4,7 @@ DAT_read <- function(file, table.name = "T_PONT*") {
     tables.start <- grep("^T_", source.data)
     tables.name <- source.data[tables.start]
     tables.name.idx <- which(tables.name == table.name)
-    table.head <- tables.start[tables.name.idx]
-    table.head
+    table.head <- tables.start[tables.name.idx] + 1
+    table.tail <- tables.start[tables.name.idx + 1] - 1
+    source.data[table.head: table.tail]
 }
