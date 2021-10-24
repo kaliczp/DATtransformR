b8b87b2628a47541641285ed98d8b7d9d4624915
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-10-24 09:03:47 +0200

    Text table is read in and angle is modified to rotated database

--- transform.R
+++ transform.R
@@ -8,3 +8,15 @@ currpoints$y <- currpoints$y + 10000
 currpoints <- eovrotate(currpoints, -75)
 
 DAT_modify_table(currpoints, "BatéOld.dat", "teszt.dat")
+
+currtext <- DAT_read(DATfiles[2], table.name = "T_FELIRAT*")
+names(currtext) <- c("Nr", "Text", "Pt.id", "Angle", "Font", "Valid", "Ref.tab", "Ref.tab.line", "Type")
+
+CurrTextAngle <- as.numeric(currtext$Angle)
+CurrTextAngle[CurrTextAngle != 90] <- CurrTextAngle[CurrTextAngle != 90] - 75
+
+CurrTextAngle[CurrTextAngle < 0] <- CurrTextAngle[CurrTextAngle < 0] + 180
+
+currtext$Angle <- round(CurrTextAngle, 1)
+
+DAT_modify_table(currtext, "teszt.dat", "teszt1.dat", table.name = "T_FELIRAT*")
