73663c76802b5f6266f43b6b8cafa5eb92795a88
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2021-10-24 19:07:45 +0200

    Formatting in DAT_read() for loop

--- DAT_read.R
+++ DAT_read.R
@@ -12,26 +12,26 @@ DAT_read <- function(file) {
     ## Read header
     database.list[[1]] <- unlist(strsplit(source.data[1], "\\*"))
     for(tables.name.idx in 1:length(tables.start)) {
-    ## Table begins after the title of the selected table
-    table.head <- tables.start[tables.name.idx] + 1
-    ## Table ends before the next title or at the last row of the file
-    next.name.idx <- tables.name.idx + 1
-    if(next.name.idx <= length(tables.start)) {
-        table.tail <- tables.start[next.name.idx] - 1
-    } else {
-        table.tail <- length(source.data)
-    }
-    ## Substract table
-    raw.table <- source.data[table.head: table.tail]
-    ## Split information at * separator
-    raw.table.atomic <- unlist(strsplit(raw.table, "\\*"))
-    ## Numeric table is converted from character in one step if flag activated
-    if(tables.name.idx < 4) {
-        raw.table.atomic <- as.numeric(raw.table.atomic)
-    }
-    ## Build data.frame from splitted data
-    table.rows <- length(raw.table)
-    database.list[[tables.name.idx + 1]] <- as.data.frame(matrix(raw.table.atomic, nrow = table.rows, byrow=TRUE))
+        ## Table begins after the title of the selected table
+        table.head <- tables.start[tables.name.idx] + 1
+        ## Table ends before the next title or at the last row of the file
+        next.name.idx <- tables.name.idx + 1
+        if(next.name.idx <= length(tables.start)) {
+            table.tail <- tables.start[next.name.idx] - 1
+        } else {
+            table.tail <- length(source.data)
+        }
+        ## Substract table
+        raw.table <- source.data[table.head: table.tail]
+        ## Split information at * separator
+        raw.table.atomic <- unlist(strsplit(raw.table, "\\*"))
+        ## Numeric table is converted from character in one step if flag activated
+        if(tables.name.idx < 4) {
+            raw.table.atomic <- as.numeric(raw.table.atomic)
+        }
+        ## Build data.frame from splitted data
+        table.rows <- length(raw.table)
+        database.list[[tables.name.idx + 1]] <- as.data.frame(matrix(raw.table.atomic, nrow = table.rows, byrow=TRUE))
     }
     ## Remove closing asterix from names
     tables.name <- sub("\\*", "", tables.name)
