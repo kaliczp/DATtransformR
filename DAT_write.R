5fbd5cfab2f6a93804bb0742ea7ec706dd194ba3
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2022-09-20 14:15:55 +0200

    Table headers is saved with carriage return
    Earlier there wasn no carriage return (\r) at the end of the table names.
    Now it is added the table names too.
    
    This commit closes #5 issue.

--- DAT_write.R
+++ DAT_write.R
@@ -3,7 +3,7 @@ DAT_write <- function(data, file) {
     write.table(t(as.data.frame(data[[1]])), connnew.file, sep = "*", eol = "*\r\n", na = "", row.names = FALSE, col.names= FALSE, quote = FALSE)
     table.names <- names(data)
     for(curr.table in 2:length(data)) {
-        writeLines(paste0(table.names[[curr.table]], "*"), connnew.file)
+        writeLines(text = paste0(table.names[[curr.table]], "*"), con = connnew.file, sep = "\r\n")
         write.table(data[[curr.table]], connnew.file, sep = "*", eol = "*\r\n", na = "", row.names = FALSE, col.names= FALSE, quote = FALSE, append = TRUE)
     }
     close(connnew.file)
