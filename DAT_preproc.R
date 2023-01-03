5de62618aee65128853d5725a41f251736f8ce60
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2023-01-03 11:29:06 +0100

    Tables headers concentrated into DAT_preproc() function
    Some table has own header which was created in transform.R. Now there are
    collected into one function (DAT_preproc) which applicable optionally.
    
    All duplications removed from transrom.R.

---
+++ DAT_preproc.R
@@ -0,0 +1,17 @@
+DAT_preproc <- function(x) {
+    ## T_PONT
+    names(x$T_PONT) <- c("Nr", "x", "y", "H", "errV", "errM")
+    ## T_FELIRAT
+    names(x$T_FELIRAT) <- c("Nr", "Text", "Pt.id", "Angle", "Font", "Valid", "Ref.tab", "Ref.tab.line", "Type")
+
+    x$T_FELIRAT$Nr <- as.numeric(x$T_FELIRAT$Nr)
+    x$T_FELIRAT$Pt.id <- as.numeric(x$T_FELIRAT$Pt.id)
+    x$T_FELIRAT$Ref.tab.line <- as.numeric(x$T_FELIRAT$Ref.tab.line)
+    x$T_FELIRAT$Type <- as.numeric(x$T_FELIRAT$Type)
+    ## T_SZIMBOLUM
+    names(x$T_SZIMBOLUM) <- c("Nr", "Key", "Pt.id", "Angle", "Ref.tab", "Ref.tab.line", "Valid")
+    x$T_SZIMBOLUM$Angle <- as.numeric(x$T_SZIMBOLUM$Angle)
+    x
+}
+
+curr.list <- DAT_preproc(curr.list)
--- transform.R
+++ transform.R
@@ -3,7 +3,6 @@
 DATfiles <- dir(patt = "dat")
 
 curr.list <- DAT_read(DATfiles[3])
-names(curr.list$T_PONT) <- c("Nr", "x", "y", "H", "errV", "errM")
 
 xshift <- 220000 - max(curr.list$T_PONT$x)
 yshift  <- 840000- max(curr.list$T_PONT$y)
@@ -12,21 +11,13 @@ curr.list$T_PONT$x <- curr.list$T_PONT$x + xshift
 curr.list$T_PONT$y <- curr.list$T_PONT$y + yshift
 curr.list$T_PONT <- round(eovrotate(curr.list$T_PONT, -73), 2)
 
-names(curr.list$T_FELIRAT) <- c("Nr", "Text", "Pt.id", "Angle", "Font", "Valid", "Ref.tab", "Ref.tab.line", "Type")
-
 CurrTextAngle <- as.numeric(curr.list$T_FELIRAT$Angle)
 CurrTextAngle[CurrTextAngle != 90] <- CurrTextAngle[CurrTextAngle != 90] - 73
 
 CurrTextAngle[CurrTextAngle < 0] <- CurrTextAngle[CurrTextAngle < 0] + 180
-
 curr.list$T_FELIRAT$Angle <- round(CurrTextAngle, 1)
-curr.list$T_FELIRAT$Nr <- as.numeric(curr.list$T_FELIRAT$Nr)
-curr.list$T_FELIRAT$Pt.id <- as.numeric(curr.list$T_FELIRAT$Pt.id)
-curr.list$T_FELIRAT$Ref.tab.line <- as.numeric(curr.list$T_FELIRAT$Ref.tab.line)
-curr.list$T_FELIRAT$Type <- as.numeric(curr.list$T_FELIRAT$Type)
 
-names(curr.list$T_SZIMBOLUM) <- c("Nr", "Key", "Pt.id", "Angle", "Ref.tab", "Ref.tab.line", "Valid")
-CurrTextAngle <- as.numeric(curr.list$T_SZIMBOLUM$Angle)
+CurrTextAngle <- curr.list$T_SZIMBOLUM$Angle
 CurrTextAngle[CurrTextAngle != 90] <- CurrTextAngle[CurrTextAngle != 90] - 73
 CurrTextAngle[CurrTextAngle < 0] <- CurrTextAngle[CurrTextAngle < 0] + 180
 curr.list$T_SZIMBOLUM$Angle <- round(CurrTextAngle, 1)
