f8fa0b17bf219be3025739ff4dd0ccc900af1766
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2023-01-03 12:46:13 +0100

    Closing asterix for every record and the header
    The DAT regulation requires a closing asterix for every data. Now both
    header and all table rows is closed with asterix. The character added
    through write.table() function eol argument.

--- DAT_write.R
+++ DAT_write.R
@@ -1,10 +1,12 @@
 DAT_write <- function(data, file) {
     connnew.file <- file(file, "w")
-    write.table(t(as.data.frame(data[[1]])), connnew.file, sep = "*", na = "", row.names = FALSE, col.names= FALSE, quote = FALSE)
+    write.table(t(as.data.frame(data[[1]])), connnew.file, sep = "*", na = "", eol = "*\n",
+                row.names = FALSE, col.names= FALSE, quote = FALSE)
     table.names <- names(data)
     for(curr.table in 2:length(data)) {
         writeLines(text = paste0(table.names[[curr.table]], "*"), con = connnew.file)
-        write.table(data[[curr.table]], connnew.file, sep = "*", na = "", row.names = FALSE, col.names= FALSE, quote = FALSE, append = TRUE)
+        write.table(data[[curr.table]], connnew.file, sep = "*", na = "", eol = "*\n",
+                    row.names = FALSE, col.names= FALSE, quote = FALSE, append = TRUE)
     }
     close(connnew.file)
 }
